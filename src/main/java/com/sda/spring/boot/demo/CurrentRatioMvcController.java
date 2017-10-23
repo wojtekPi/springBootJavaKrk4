e2dc7aa678d82afbd93604e2d596e0ae588c05ff
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2017-10-23 20:14:45 +0200

    added template!

---
+++ src/main/java/com/sda/spring/boot/demo/CurrentRatioMvcController.java
@@ -0,0 +1,22 @@
+package com.sda.spring.boot.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CurrentRatioMvcController {
+
+    private final CurrencyRatioRepository currencyRatioRepository;
+
+    public CurrentRatioMvcController(CurrencyRatioRepository currencyRatioRepository) {
+        this.currencyRatioRepository = currencyRatioRepository;
+    }
+
+    @RequestMapping("/currentCurrencyRatios")
+    String currentRatios(Model model) {
+        model.addAttribute("currentRatios", this.currencyRatioRepository.findAll());
+        return "currentRatiosSite";
+    }
+}
+
